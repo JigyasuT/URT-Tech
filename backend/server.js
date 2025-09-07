0b5ae025b0f134c7690c49ac206c44de6d0410a1
JigyasuT <jigyasuti@gmail.com>
2025-09-07 13:08:22 +0530

    final commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+// import connectDB from "./utils/db.js";
+import propertyRoutes from "./routes/propertyRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./utils/db.js";
+import cookieParser from "cookie-parser";
+// import serverless from "serverless-http";
+
+dotenv.config();
+connectDB()
+
+const app = express();
+
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/properties", propertyRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+
+
+
