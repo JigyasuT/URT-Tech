c9b7b765e44d214def3c755495f001f5216d56f2
JigyasuT <jigyasuti@gmail.com>
2025-09-07 12:56:30 +0530

    6th commit

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/properties", propertyRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/properties", propertyRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
